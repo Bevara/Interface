31b88967dcc8b473979e5115b375a1bca1e3b67a
jgn <gorinje@gmail.com>
2023-06-15 18:43:52 +0200

    Saving work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +8,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { PreviewComponent } from './preview/preview.component';
 import { PreserveComponent } from './preserve/preserve.component';
 import { DevelopComponent } from './develop/develop.component';
@@ -26,7 +28,9 @@ import { DevelopComponent } from './develop/develop.component';
     BrowserAnimationsModule,
     MatSidenavModule,
     MatListModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/preserve/preserve.component.ts
+++ src/app/preserve/preserve.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreserveComponent implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit(): void {
