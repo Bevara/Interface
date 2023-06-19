d38d19a224d43be4a2949db6b3c0b5b899fac961
jgn <gorinje@gmail.com>
2023-06-19 13:01:06 +0200

    Saving work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatLegacyListModule as MatListModule} from '@angular/material/legacy-list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatLegacyChipsModule as MatChipsModule} from '@angular/material/legacy-chips';
-import {MatLegacyFormFieldModule as MatFormFieldModule} from '@angular/material/legacy-form-field';
 import { PreviewComponent } from './preview/preview.component';
 import { PreserveComponent } from './preserve/preserve.component';
 import { DevelopComponent } from './develop/develop.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatLegacyListModule as MatListModule} from '@angular/material/legacy-list';
+import {CdkDrag, CdkDropList} from '@angular/cdk/drag-drop';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -30,7 +33,11 @@ import { DevelopComponent } from './develop/develop.component';
     MatListModule,
     MatIconModule,
     MatFormFieldModule,
-    MatChipsModule
+    MatChipsModule,
+    CdkDropList, 
+    CdkDrag,
+    MatCardModule, 
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/preserve/preserve.component.ts
+++ src/app/preserve/preserve.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {MatChipEditedEvent, MatChipInputEvent, MatChipsModule} from '@angular/material/chips';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
+export interface Fruit {
+  name: string;
+}
 
 @Component({
   selector: 'app-preserve',
@@ -7,7 +15,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreserveComponent implements OnInit {
 
-  
+  addOnBlur = true;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  fruits: Fruit[] = [{name: 'avidmx'}, {name: 'xvid'}, {name: 'libmad'}, {name: 'rfmpgvid'}];
+
+  announcer = inject(LiveAnnouncer);
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value) {
+      this.fruits.push({name: value});
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
+
+  remove(fruit: Fruit): void {
+    const index = this.fruits.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+
+      this.announcer.announce(`Removed ${fruit}`);
+    }
+  }
+
+  edit(fruit: Fruit, event: MatChipEditedEvent) {
+    const value = event.value.trim();
+
+    // Remove fruit if it no longer has a name
+    if (!value) {
+      this.remove(fruit);
+      return;
+    }
+
+    // Edit existing fruit
+    const index = this.fruits.indexOf(fruit);
+    if (index >= 0) {
+      this.fruits[index].name = value;
+    }
+  }
+
+  drop(event: CdkDragDrop<Fruit[]>) {
+    moveItemInArray(this.fruits, event.previousIndex, event.currentIndex);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
